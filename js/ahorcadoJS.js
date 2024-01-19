01c8f9e2d48bb8af057ebf5faae3336321f00317
Informatica <informatica@alumno.local>
2024-01-19 09:18:01 -0600

    Trabajo Finalizado

--- js/ahorcadoJS.js
+++ js/ahorcadoJS.js
@@ -6,23 +6,22 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const intentosLabel = document.getElementById("intentos-restantes");
 
-//Selecciona una palabra
+
 function seleccionarPalabra(palabras){
     var total_palabras = palabras.length -1;
     var rand = (Math.random() * total_palabras).toFixed(0);
     palabra_seleccionada = palabras[rand].toLowerCase();
-    console.log(palabra_seleccionada);
     pintarPalabra(palabra_seleccionada);
 }
 
-// función para verificar si ya se utilizo una letra
+
 function chequear(event){
     comprobarLetra(event.target.textContent);
     
 }
 
 
-//Pinta las letras
+
 function pintarPalabra(palabra){
     var str = '';
     var letras = palabra.split('');
@@ -39,14 +38,15 @@ function pintarPalabra(palabra){
     document.getElementById("word").innerHTML = str;
 }
 
-//Actualiza los intentos de la etiqueta <p>
+
 function actualizarIntentos() {
-    intentosLabel.textContent = intentos.toString(); // Convierte el valor a texto y actualiza el contenido
+    intentosLabel.textContent = intentos.toString(); 
 }
-//genera el teclado
+
 function abc(){
     var a = 97;
     var z = 123;
+    
     var letras = document.getElementById("letras");
     for(var l = a; l < z ; l++){
         const char = String.fromCharCode(l);
@@ -55,14 +55,15 @@ function abc(){
         letra.classList.add('abc', 'bg-secondary', 'text-primary', 'manito');
         letra.setAttribute('id', 'letra-' + char);
         letra.textContent = char;
-        //añade el evento a la tecla
+        
         letra.addEventListener('click', chequear);
         
         letras.appendChild(letra);
     }
+    
 }
-//CANVAS
-function drawHangman() {
+
+function dibujo() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.moveTo(100, 20);
@@ -134,9 +135,9 @@ function mostrarResultado(ganador) {
     const nuevoJuego = document.getElementById("nuevo-juego");
 
     if (ganador) {
-        resultadoTexto.textContent = "Haz ganado, presiona el botón volver a iniciar, La palabra es:"  + palabra_seleccionada;
+        resultadoTexto.textContent = "Haz ganado, La palabra es:"  + palabra_seleccionada;
     } else {
-        resultadoTexto.textContent = "haz perdido, presiona el botón para una nueva partida, La palabra era: "+ palabra_seleccionada;
+        resultadoTexto.textContent = "haz perdido, La palabra era: "+ palabra_seleccionada;
     }
 
     resultadoPopup.classList.remove("hide");
@@ -148,7 +149,7 @@ function mostrarResultado(ganador) {
     });
 }
 
-//comprueba la letra si es correcta o no
+
 function comprobarLetra(char){
     var letra = document.getElementById('letra-'+ char);
 
@@ -164,32 +165,30 @@ function comprobarLetra(char){
         intentos--; 
         fallos++; 
         actualizarIntentos();
-        letra.classList.remove("bg-secondary", "text-primary"); 
-        letra.classList.add("bg-dark", 'text-light');
-       drawHangman();
+        letra.classList.remove("bg-secondary"); 
+        letra.classList.add("bg-dark");
+        dibujo();
         
     }
     letra.classList.toggle("manito");
-    //remueve el vento de la tecla
+    
     letra.removeEventListener('click', chequear);
-    //verifica si la palabra fue completada o no
+    
     comprobarPalabra();
 
 }
 
 
-//Comprobar los intentos y recarga la página
+
 function comprobarPalabra(){
     if(intentos == 0){
-       //alert('Haz perdido, presiona el botón volver a iniciar ' + 'La palabra era: ' + palabra_seleccionada )
+       
         mostrarResultado(false);
-      //  window.location.reload();
-        //start();
+     
     }else if(concordancias.indexOf("_") == -1){
-       // alert('haz GANADO, presiona el botón para una nueva partida, '+ 'La palabra es: '+ palabra_seleccionada)
+
        mostrarResultado(true);
-        //window.location.reload();
-        //start();
+     
     }
 }
 
